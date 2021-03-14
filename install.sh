085546ba6449a864fb38f9d1b056721d7885ffd1
Eduardo Faria <z.eduardofaria@gmail.com>
2021-03-14 20:03:13 -0300

    Update install.sh
    adding xclip

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ sudo apt-get install -y python-dnspython
 sudo apt-get install -y git
 sudo apt-get install -y rename
 sudo apt-get install -y xargs
+sudo apt-get install -y xclip
 
 #install go
 if [[ -z "$GOPATH" ]];then
